64f9e0a77d2927e7f3e113604b304c9e9def500e
Mayur Garg <mayur7garg@gmail.com>
2021-08-01 20:13:27 +0000

    Added data cleaning steps

---
+++ Data Preprocessing/data_preprocessing_util.py
@@ -0,0 +1,20 @@
+import string
+import nltk
+from nltk.corpus import stopwords
+
+nltk.download('stopwords')
+
+UNWANTED_CHARS = set(string.punctuation) | set("\n\t\r")
+
+en_stopwords = set(stopwords.words('english'))
+
+
+def clean_text(text):
+    chars = [char for char in text.lower() if char not in UNWANTED_CHARS]
+
+    no_punc_text = ''.join(chars)
+
+    words = [word for word in no_punc_text.split(' ') if not (
+        word in en_stopwords or word.isspace() or word.isnumeric())]
+
+    return ' '.join(words)
